e523fd16ca376fc5bbf41e6d6a20b71be4d680d6
aserafim <correia.alefe@gmail.com>
2024-10-28 11:07:39 -0300

    :sparkles: feat: Entendendo storage e memory

---
+++ src/storage_vs_memory.sol
@@ -0,0 +1,20 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+contract A{
+    string[] public cities = ["Osasco", "Carapicuiba"];
+
+    function f_memory() public{
+        string[] memory s1 = cities;
+        // string s2; -> error
+        s1[0] = "Barueri";
+    }
+
+    function f_storage() public{
+        string[] storage s1 = cities;
+        // string s2; -> error
+        s1[0] = "Barueri";
+    }
+
+}
