112ecc531ca3cdce041046cc925c5f92ceb20d3c
aserafim <correia.alefe@gmail.com>
2024-10-29 16:19:37 -0300

    Trabalhando com funções built in

---
+++ src/contract_address_balance.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+contract Deposit{
+
+    receive() external payable { }
+    fallback() external payable{ }
+
+    function getBalance() public view returns(uint){
+        return address(this).balance;
+    }
+
+    function sendEther() public payable {
+
+    }
+
+    function transferEther(address payable recipient, uint amount) public returns(bool){
+        if (amount <= getBalance()){
+            recipient.transfer(amount);
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
